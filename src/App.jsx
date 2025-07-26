ce8183c97f82072f587e9de8683611a9c913182d
Eurus <exampl@gamil.com>
2025-07-26 18:23:21 +0500

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from './component/Header/Header'
-import Section from './component/section/section'
+import Section from './component/Section/Section'
 import Footer from './component/Footer/Footer'
 
 function App() {
